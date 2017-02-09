42ae7192c591ca38b55712f06421a6889f40883f
Alexandre Duros <alexandre.duros@gmail.com>
2017-02-09 23:51:16 +0100

    add first component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="emk">
+        <div className="emk-header">
+          <h1>e-marque</h1>
+          <h2>Bienvenue dans l'application</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
